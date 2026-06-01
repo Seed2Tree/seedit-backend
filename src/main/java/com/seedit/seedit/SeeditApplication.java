fba22410672c1bfb37722a9b14baea91e3a36133
hyunneee <sh_oh@u.sogang.ac.kr>
2026-06-01 15:02:33 +0900

    [chore] set up spring boot pjt - SEED-13

---
+++ src/main/java/com/seedit/seedit/SeeditApplication.java
@@ -0,0 +1,13 @@
+package com.seedit.seedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seedit/seedit/SeeditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seedit.seedit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
